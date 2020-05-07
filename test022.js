0b2fcb0c4da1e087c392ef5d829c0dda7277f80f
chrisloarryn <cristobal.contreras11@inacapmail.cl>
2020-05-07 01:00:18 -0400

    Create test022.js

---
+++ test022.js
@@ -0,0 +1,15 @@
+// Write a function that takes an array of Random Integers as the input
+// and returns an array where all the Even numbers are at the beginning and
+// all the Odd numbers are at the end. Extra points for answers
+
+// Random integers
+const dummyData = [20, 23, 33, 44, 13, 11, 8, 7, 6, 5, 4, 3, 2, 1]
+
+/**
+
+ */
+const numbersReturned = (nums) => {
+    console.log(nums)
+}
+
+numbersReturned(dummyData)
