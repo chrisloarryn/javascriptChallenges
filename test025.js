54f6c03b849c0b577da6f712033691f71b8d0d2a
chrisloarryn <chrisloarryn@gmail.com>
2020-06-19 15:01:47 -0400

    Create test025.js

---
+++ test025.js
@@ -0,0 +1,27 @@
+/*
+Given the size of the sides of an array, you are asked to mark with an x ​​the elements of the diagonals and with a _ all the others
+
+input: int side
+output: array diagonals
+ 
+ diagonals(3)
+ 
+ [['x','_','x'],
+  ['_','x','_'],
+  ['x','_','x']]
+
+*/
+const diagonals = sides => {
+    var side = [];
+    for(let i = 2; i < sides+2; i++) side.push(i%2===0 ? ['x','_','x'] : ['_','x','_'])
+    return side;
+}
+
+console.log("5x5")
+diagonals(5).forEach(x => console.log(x))
+
+console.log("11x11")
+diagonals(11).forEach(x=>console.log(x))
+
+console.log("15x15")
+diagonals(15).forEach(x=>console.log(x))
