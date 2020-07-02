1c41867b242aaa613cdc9b785e6f395240190dfb
chrisloarryn <chrisloarryn@gmail.com>
2020-07-02 00:28:13 -0400

    Update test025.js

--- test025.js
+++ test025.js
@@ -11,17 +11,28 @@ output: array diagonals
   ['x','_','x']]
 
 */
-const diagonals = sides => {
-    var side = [];
-    for(let i = 2; i < sides+2; i++) side.push(i%2===0 ? ['x','_','x'] : ['_','x','_'])
-    return side;
+// const diagonals = sides => {
+//     var side = [];
+//     for(let i = 2; i < sides+2; i++) side.push(i%2===0 ? ['x','_','x'] : ['_','x','_'])
+//     return side;
+// }
+
+const range = (start, end) => {
+    const array = [];
+    for (let i = start; i < end; i++) array.push(i)
+    return array
+}
+
+function diagonals(side) {
+    const ar = range(0, side)
+    return ar.map(i => ar.map(e => (e === i || side - e === i ? 'x' : '_')))
 }
 
 console.log("5x5")
 diagonals(5).forEach(x => console.log(x))
 
 console.log("11x11")
-diagonals(11).forEach(x=>console.log(x))
+diagonals(11).forEach(x => console.log(x))
 
 console.log("15x15")
-diagonals(15).forEach(x=>console.log(x))
+diagonals(15).forEach(x => console.log(x))
