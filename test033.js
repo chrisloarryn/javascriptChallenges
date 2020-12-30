cdabc48667b1bac1a6364be1cb2149b8029b2a98
chrisloarryn <chrisloarryn@gmail.com>
2020-12-30 07:00:07 -0300

    test033.js
    recursivity

--- test033.js
+++ test033.js
@@ -20,3 +20,21 @@ function countDownFrom(number) {
 
 countDownFrom(5)
 // => !dlrow olleh
+
+console.log('another countdown')
+
+// program to count down numbers to 1
+function countDown(number) {
+  // display the number
+  console.log(number)
+
+  // decrease the number value
+  const newNumber = number - 1
+
+  // base case
+  if (newNumber > 0) {
+    countDown(newNumber)
+  }
+}
+
+countDown(4)
