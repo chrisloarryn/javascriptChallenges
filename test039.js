f092e82c7b3167d4bba1f1829dbc9e5e0de92d4e
chrisloarryn <chrisloarryn@gmail.com>
2021-01-30 13:24:36 -0300

    up

---
+++ test039.js
@@ -0,0 +1,31 @@
+const Axios = require('axios')
+
+const urlBase = 'api.yelp.com'
+const apiVersion = 'v3'
+const protocol = 'https'
+const query = 'businesses'
+const clientId = '1WzOFQN6owpL375zFZrdbg'
+const token =
+  'xIwPcgqa1AOBLPTMothrSanLU7wbzoQqZ8H0AjxbWX8rC2MzMxLNJeDAQmsZ9JSgF02T2JECe5wB8lYCEshkoei5GzRrlOf2cl98xfUf9TzbAQBwZnMI9ijflWwTYHYx'
+const find = 'north-india-restaurant-san-francisco'
+const url = `${protocol}://${urlBase}/${query}/${find}`
+
+const axios = Axios.create({
+  baseUrl: `${protocol}://${urlBase}/${apiVersion}`,
+  timeout: 1000,
+  headers: {
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+})
+
+const fetch = async () => {
+  const res = axios
+    .get(`/${query}/${find}`)
+    .then(res => res)
+    .catch(rej => `${rej.response.status} - ${rej.response.statusText}`)
+  console.log(await res)
+}
+
+fetch()
