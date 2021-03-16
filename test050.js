88eb2fc963b218a541f7322bc58d0d0f8d992c59
chrisloarryn <chrisloarryn@gmail.com>
2021-03-16 17:18:27 -0300

    Update test050.js

--- test050.js
+++ test050.js
@@ -42,8 +42,50 @@ const newItems = [
   },
 ]
 
-const oldItemsArray = lodash.map(oldItems, '_id')
-const newItemsArray = lodash.map(newItems, '_id')
+const newItems1 = [
+  {
+    _id: '604f4ada3cb009c7b23cef07',
+    service: '60116105db99454264a956f7',
+    status: 'PENDING',
+    realizationDate: undefined, // '2021-03-11T17:53:04.350Z'
+    startDatePrivilege: undefined,
+    endDatePrivilege: undefined,
+    document: undefined,
+  },
+  {
+    _id: '604f4ada3cb009c7b23cef06',
+    service: '60116105db99454264a956fc',
+    status: 'PENDING',
+    realizationDate: '2021-03-11T17:53:04.350Z',
+    startDatePrivilege: '2021-03-11T17:53:04.350Z',
+    endDatePrivilege: '2021-03-11T17:53:04.350Z',
+    document: 'undefined',
+  },
+]
+
+const newItems2 = [
+  {
+    _id: '604f4ada3cb009c7b23cef07',
+    service: '60116105db99454264a956f7',
+    status: 'PENDING',
+    realizationDate: undefined, // '2021-03-11T17:53:04.350Z'
+    startDatePrivilege: undefined,
+    endDatePrivilege: undefined,
+    document: undefined,
+  },
+  {
+    _id: '604f4ada3cb009c7b23cef06',
+    service: '60116105db99454264a956fc',
+    status: 'PENDINGOS',
+    realizationDate: '2021-03-11T17:53:04.350Z',
+    startDatePrivilege: '2021-03-11T17:53:04.350Z',
+    endDatePrivilege: '2021-03-11T17:53:04.350Z',
+    document: 'undefined',
+  },
+]
+
+const oldItemsArray = lodash.map(newItems1, '_id')
+const newItemsArray = lodash.map(newItems2, '_id')
 const servicePickOnNewAndDelete = [
   '_id',
   'service',
@@ -62,7 +104,7 @@ const serviceOmitInModify = [
 ]
 
 // news
-if (lodash.difference(newItemsArray, oldItemsArray)) {
+if (lodash.difference(newItems2, newItems1)) {
   const itemsToAdd = lodash.difference(newItemsArray, oldItemsArray)
   const findElement = itemsToAdd
     .map(item => ({
@@ -72,10 +114,12 @@ if (lodash.difference(newItemsArray, oldItemsArray)) {
   console.log('commit add', findElement)
 }
 
-// modified
-if (lodash.difference(oldItems, newItems)) {
+// modified items
+if (
+  lodash.difference(newItems1, newItems2) &&
+  !lodash.isEqual(newItems1, newItems2)
+) {
   const item = oldItems && oldItems.length > 0 ? oldItems[0] : undefined
-
   if (!lodash.isEmpty(item)) {
     const pickedItem = lodash.omit(item, serviceOmitInModify)
     const keys = lodash.keysIn(pickedItem)
@@ -84,19 +128,32 @@ if (lodash.difference(oldItems, newItems)) {
       const [item] = lodash.differenceBy(newItems, oldItems, el)
       return item
     })
-    const mergedArray = lodash.merge(arrayToMerge)
-    console.log('modified', mergedArray)
-    // lodash.uniqBy(mergedArray, '_id')
+    var merged = lodash.uniqBy(arrayToMerge, '_id')
+    var values = lodash.values(merged)
+    // console.log(values)
+    // const mergedArray = lodash.merge() // .filter(el => el)
+    /*mergedArray.forEach(async el => {
+      console.log(el)
+       await addHistoryEntry(conn, {
+        event: HistoryEventsEnum.ServiceModified,
+        name: HistoryEntitiesEnum.Services,
+        _id: professional._id,
+        createdBy: user._id,
+        meta: meta as HistoryMeta[],
+        otherId: new Types.ObjectId(el._id),
+      }) 
+    })*/
   }
 }
 
 // deleted
-if (lodash.difference(newItemsArray, oldItemsArray)) {
+if (lodash.difference(newItems2, newItems1)) {
   const itemsToDelete = lodash.difference(oldItemsArray, newItemsArray)
-  const findElement = itemsToDelete
-    .map(item => ({
-      ...oldItems.find(el => el._id === item),
-    }))
-    .map(el => lodash.pick(el, servicePickOnNewAndDelete))
+  console.log(itemsToDelete)
+  const findElement = itemsToDelete.map(item => ({
+    ...newItems1.find(el => el._id === item),
+  }))
+  // console.log(findElement)
+  // .map(el => lodash.pick(el, servicePickOnNewAndDelete))
   console.log('commit delete', findElement)
 }
