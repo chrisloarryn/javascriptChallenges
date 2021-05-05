6c5104fc2a7f39c8b936cc3eefacb590d59c57d7
chrisloarryn <chrisloarryn@gmail.com>
2021-05-05 10:52:40 -0400

    Create test056.js

---
+++ test056.js
@@ -0,0 +1,23 @@
+let s = new Set()
+
+s.add(1)
+s.add(2)
+s.add(3)
+s.add('3')
+s.add('3')
+
+console.log(s)
+
+const list1 = [1, 2, 3],
+  list2 = [5, 6, 7, 8]
+
+const concatened = list1.concat(list2)
+
+let count = 0
+for (let i in [3, 4, 5, 6]) count += 1
+
+console.log(count)
+
+const name = 'Harry'
+
+console.log(name[0])
