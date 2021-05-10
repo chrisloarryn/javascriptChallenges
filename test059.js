87a5e3f9fe361dc2bff9c7b5e1ea656b60bb84e9
Cristóbal Contreras <44170429+chrisloarryn@users.noreply.github.com>
2021-05-10 09:27:34 -0400

    Create test059.js

---
+++ test059.js
@@ -0,0 +1,10 @@
+const objElement = {
+  date: '2021-05-06',
+  name: 'atrik',
+  paymentMode: 'cash',
+  price: ['200', '2255'],
+  product: ['muscle', 'safasf'],
+  unit: ['200', '2255'],
+}
+
+console.log(Array.from(objElement))
